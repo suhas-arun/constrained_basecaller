66a1f5d9bfc81eff1065a620b74094ddacaf9cb4
sa2021 <sa2021@ic.ac.uk>
2025-04-23 17:23:32 +0100

    move basecaller output to out/ directory

--- baseline.sh
+++ baseline.sh
@@ -3,11 +3,11 @@
 
 MODEL=bonito/models/dna_r10.4.1_e8.2_400bps_sup@v5.0.0/
 DATA_PATH=../data
-OUTPUT_FILENAME=baseline_bonito.fastq
+OUTPUT_PATH=../out/baseline_bonito.fastq
 
 source /vol/bitbucket/sa2021/miniconda3/etc/profile.d/conda.sh
 
 conda activate bonito-env
 cd bonito
-bonito basecaller $MODEL $DATA_PATH > $DATA_PATH/$OUTPUT_FILENAME
+bonito basecaller $MODEL $DATA_PATH > $OUTPUT_PATH
 conda deactivate
