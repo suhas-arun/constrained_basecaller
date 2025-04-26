37b2f719680e681deb56ffb8f7e10386e0cacdf6
sa2021 <sa2021@ic.ac.uk>
2025-04-26 15:01:33 +0100

    split blow5 file to parallelise conversion to fast5

--- basecall.sh
+++ basecall.sh
@@ -4,7 +4,7 @@
 ##################################################
 
 # Data generation parameters
-NUM_SEQUENCES=10000
+NUM_SEQUENCES=100000
 SEQUENCE_LENGTH=500
 MAX_HOMOPOLYMER_LENGTH=3
 MIN_GC=0.4
@@ -12,13 +12,10 @@ MAX_GC=0.6
 
 # File paths
 ROOT_DIR=$(pwd)
-mkdir -p "$ROOT_DIR/data"
-mkdir -p "$ROOT_DIR/out"
 FASTA_FILE="$ROOT_DIR/data/mock_data.fasta"
 BLOW5_FILE="$ROOT_DIR/data/reads.blow5"
-FAST5_DIR="$ROOT_DIR/data/squigulator"
-mkdir -p $FAST5_DIR
-FAST5_FILE="$FAST5_DIR/squigulator.fast5"
+BLOW5_DIR="$ROOT_DIR/data/blow5"
+FAST5_DIR="$ROOT_DIR/data/fast5"
 SAM_FILE="$ROOT_DIR/out/basecalls.sam"
 
 # Squigulator parameters
@@ -29,6 +26,7 @@ DNA_PROFILE="dna-r10-prom"
 # Slow5tools parameters
 SLOW5TOOLS_VERSION="v1.3.0"
 SLOW5TOOLS_DIR="$ROOT_DIR/lib/slow5tools-$SLOW5TOOLS_VERSION"
+NUM_THREADS=8
 
 # Baseline model parameters
 MODEL="bonito/models/dna_r10.4.1_e8.2_400bps_sup@v5.0.0/"
@@ -36,6 +34,12 @@ MODEL="bonito/models/dna_r10.4.1_e8.2_400bps_sup@v5.0.0/"
 ##################################################
 
 source /vol/bitbucket/sa2021/miniconda3/etc/profile.d/conda.sh
+
+# Clean up previous runs
+rm -rf "$ROOT_DIR/data"
+rm -rf "$ROOT_DIR/out"
+mkdir -p "$ROOT_DIR/data"
+mkdir -p "$ROOT_DIR/out"
 
 # Generate FASTA file
 conda activate bonito-env
@@ -55,8 +59,14 @@ $SQUIGULATOR_DIR/squigulator \
     -x $DNA_PROFILE \
     -o $BLOW5_FILE \
 
+# Split BLOW5 file for parallel processing
+$SLOW5TOOLS_DIR/slow5tools split \
+    $BLOW5_FILE \
+    -d $BLOW5_DIR \
+    -r $((NUM_SEQUENCES / NUM_THREADS + 1))
+    
 # Convert BLOW5 to FAST5
-$SLOW5TOOLS_DIR/slow5tools s2f $BLOW5_FILE -o $FAST5_FILE
+$SLOW5TOOLS_DIR/slow5tools s2f $BLOW5_DIR -d $FAST5_DIR
 
 # Perform basecalling using Bonito
 cd bonito
@@ -66,6 +76,7 @@ bonito basecaller \
 
 # Clean up
 rm -rf $BLOW5_FILE
+rm -rf $BLOW5_DIR
 rm -rf $FAST5_DIR
 
 conda deactivate
