81f09317a910ded2a3f65a95c2c082e9afeee52a
sa2021 <sa2021@ic.ac.uk>
2025-05-12 01:16:13 +0100

    set up basecalling training pipeline

--- constrained_basecall.sh
+++ constrained_basecall.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 # Perform basecalling with the constraint-aware basecaller model
 
+##################################################
+
 ROOT_DIR=$(pwd)
 FASTA_FILE="$ROOT_DIR/data/reference.fasta"
 FAST5_DIR="$ROOT_DIR/data/fast5"
 SAM_FILE="$ROOT_DIR/out/basecalls.sam"
 
+# Baseline model parameters
+MODEL="dna_r10.4.1_e8.2_400bps_sup@v5.0.0/"
+
+##################################################
+
 source /vol/bitbucket/sa2021/miniconda3/etc/profile.d/conda.sh
 conda activate bonito-env
+export PYTHONPATH="bonito:src"
 
 read -p "Do you want to generate new data? (y/n): " confirm
 if [[ $confirm != "y" && $confirm != "Y" ]]; then
@@ -16,11 +24,45 @@ else
     ./generate_data.sh $FASTA_FILE $FAST5_DIR
 fi
 
-PYTHONPATH="bonito:src" \
-    python3 -m basecaller.main \
-        $FAST5_DIR \
-        $FASTA_FILE \
-        > $SAM_FILE
+read -p "Do you want to train the model? (y/n): " train
+if [[ $train != "y" && $train != "Y" ]]; then
+    echo "Using pre-trained model."
+else
+    TRAINING_DIR="$ROOT_DIR/data/train"
+    TRAINING_DATA="$TRAINING_DIR/basecalls.sam"
+    OUTPUT_DIR="$ROOT_DIR/out/constrained"
+
+    # Training hyperparameters
+    EPOCHS=20
+    CHUNKS=400
+    VALID_CHUNKS=20
+    BATCH_SIZE=16
+
+    mkdir -p $TRAINING_DIR
+    rm -rf $OUTPUT_DIR
+
+    echo "Preparaing training data..."
+    bonito basecaller \
+        --reference $FASTA_FILE \
+        --save-ct \
+        --min-accuracy-save-ctc 0.8 \
+        $MODEL $FAST5_DIR > $TRAINING_DATA
+
+    python3 -m basecaller.train \
+        --training-directory $OUTPUT_DIR \
+        --directory $TRAINING_DIR \
+        --epochs $EPOCHS \
+        --chunks $CHUNKS \
+        --valid-chunks $VALID_CHUNKS \
+        --batch $BATCH_SIZE
+
+    cd $ROOT_DIR
+fi
+
+python3 -m basecaller.main \
+    $FAST5_DIR \
+    $FASTA_FILE \
+    > $SAM_FILE
 
 # Analyse basecalling results
 echo "Analysing basecalling results..."
---
+++ src/basecaller/train.py
@@ -0,0 +1,111 @@
+import os
+from bonito.data import ComputeSettings, DataSettings, ModelSetup, load_data
+from bonito.training import Trainer
+import toml
+import torch
+
+from basecaller.model import ConstraintAwareBasecaller
+
+
+def main(args):
+    workdir = os.path.expanduser(args.training_directory)
+    os.makedirs(workdir, exist_ok=True)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    print("[initializing custom model]")
+    model = ConstraintAwareBasecaller()
+    model = model.to(device)
+
+    config = {
+        "standardisation": {},  # Update this if you're using signal normalization
+        "optim": {"betas": [0.9, 0.999], "eps": 1e-08, "weight_decay": 0.0},
+    }
+
+    argsdict = dict(training=vars(args))
+    argsdict["training"]["pwd"] = os.getcwd()
+    toml.dump({**config, **argsdict}, open(os.path.join(workdir, "config.toml"), "w"))
+
+    data = DataSettings(
+        training_data=args.directory,
+        num_train_chunks=args.chunks,
+        num_valid_chunks=args.valid_chunks,
+        output_dir=workdir,
+    )
+    model_setup = ModelSetup(
+        n_pre_context_bases=getattr(model, "n_pre_context_bases", 0),
+        n_post_context_bases=getattr(model, "n_post_context_bases", 0),
+        standardisation=config["standardisation"],
+    )
+    compute_settings = ComputeSettings(
+        batch_size=args.batch,
+        num_workers=4,
+        seed=25,
+    )
+
+    print("[loading data]")
+    train_loader, valid_loader = load_data(data, model_setup, compute_settings)
+
+    trainer = Trainer(
+        model=model,
+        device=device,
+        train_loader=train_loader,
+        valid_loader=valid_loader,
+        quantile_grad_clip=True,
+        chunks_per_epoch=args.chunks,
+        batch_size=args.batch,
+    )
+
+    lr = (
+        float(args.lr) if "," not in args.lr else [float(x) for x in args.lr.split(",")]
+    )
+    trainer.fit(workdir, args.epochs, lr, **config["optim"])
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--training-directory",
+        type=str,
+        help="Directory to save training data and checkpoints",
+    )
+    parser.add_argument(
+        "--directory",
+        type=str,
+        help="Directory containing training data",
+    )
+    parser.add_argument(
+        "--chunks",
+        type=int,
+        default=1000,
+        help="Number of chunks to use for training",
+    )
+    parser.add_argument(
+        "--valid-chunks",
+        type=int,
+        default=100,
+        help="Number of chunks to use for validation",
+    )
+    parser.add_argument(
+        "--batch",
+        type=int,
+        default=32,
+        help="Batch size for training",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=10,
+        help="Number of epochs to train for",
+    )
+    parser.add_argument(
+        "--lr",
+        type=str,
+        default="0.001",
+        help="Learning rate for training",
+    )
+    args = parser.parse_args()
+
+    main(args)
